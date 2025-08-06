f153a7f9250efcff210d04bce2abd133f6486d1c
Chris Li <cli@lotus-creations.com>
2025-08-06 18:31:57 +0800

    all in one workspace

--- desktop-startup.sh
+++ desktop-startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i3-msg 'workspace 1; exec google-chrome'
-i3-msg 'workspace 2; exec /opt/cursor/cursor.appimage'
-i3-msg 'workspace 3; exec dbeaver'
-i3-msg 'workspace 4; exec subl'
-i3-msg 'workspace 5; exec nautilus'
-i3-msg 'workspace 9; exec brave-browser'
+i3-msg 'workspace 1; exec /opt/cursor/cursor.appimage'
+i3-msg 'workspace 1; exec dbeaver'
+i3-msg 'workspace 1; exec subl'
+i3-msg 'workspace 1; exec nautilus'
+i3-msg 'workspace 1; exec brave-browser'
