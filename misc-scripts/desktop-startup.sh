68d647566b06c48739fd9056afaf1095ffa9f48d
Chris Li <cli@lotus-creations.com>
2025-09-13 00:35:06 +0800

    misc scripts for linux settings

---
+++ misc-scripts/desktop-startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+i3-msg 'workspace 1; exec google-chrome'
+i3-msg 'workspace 1; exec /opt/cursor/cursor.appimage'
+i3-msg 'workspace 1; exec dbeaver'
+i3-msg 'workspace 1; exec subl'
+i3-msg 'workspace 1; exec nautilus'
+i3-msg 'workspace 1; exec brave-browser'
